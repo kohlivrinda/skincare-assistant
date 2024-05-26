8b423f811730d108cdcbc19a02f32714cf57de7c
Vortexx2 <janmayshuklay@gmail.com>
2024-05-26 15:29:58 +0530

    Setup fetching

--- src/main.py
+++ src/main.py
@@ -6,6 +6,23 @@ def img_proc(img):
     w, h = img.size    
     return f"size: {w}x{h}"
 
+
+def match_according_to_skin_type(products_targets_df, requirements_of_user, serialised_products):
+    skin_matches = products_targets_df.filter(
+        pl.col("skin type") == requirements_of_user["skin type"]
+    )
+
+    all_filtered = skin_matches.filter(
+        pl.col("acne") == requirements_of_user["acne"],
+        pl.col("dark circles") == requirements_of_user["dark circles"],
+        pl.col("acne scarring") == requirements_of_user["acne scarring"],
+        pl.col("wrinkles") == requirements_of_user["wrinkles"],
+        pl.col("pores") == requirements_of_user["pores"],
+    )
+
+    return np.array(serialised_products)[all_filtered["index"].to_numpy()]
+
+
 def main():
     st.title("Skincare Shopping Assistant")
     uploaded_img = st.file_uploader(label = 'Upload face image', type=['jpg', 'jpeg', 'png'], accept_multiple_files= False)
